9cd610301c66a042f7526af1fba4eddf0ea92bc9
Birute Petrauskienė <petrauskiene.birute@gmail.com>
2021-11-30 08:41:55 +0200

    git21

--- git2.js
+++ git2.js
@@ -1,1 +1,17 @@
 console.log("git2")
+
+document.querySelector("h1").textContent="I am"
+
+
+const headerA =document.createElement("headerA")
+headerA.textContent = "bye"
+headerA.style.color ="red"
+document.querySelector(".container").prepend(headerA)
+
+
+ const header = document.createElement("header")
+header.textContent ="Hello"
+header.style.color ="blue"
+document.querySelector(".box").append(header)
+
+// ====================================================================
